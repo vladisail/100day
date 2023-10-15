e45a07ec92f008d300dd0c1285f3ce3a6cb61a4e
vladisail <74975366+vladisail@users.noreply.github.com>
2023-10-15 14:54:13 +0600

    Delete accounts directory

--- accounts/context_processors.py
+++
@@ -1,4 +0,0 @@
-def current_user(request):
-    if request.user.is_authenticated:
-        return {'current_user': request.user}
-    return {'current_user': None}
--- accounts/main/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    return render(request, 'main/index.html')
-
-
-def course_assignments(request):
-    return render(request, 'main/course_assignments.html')
